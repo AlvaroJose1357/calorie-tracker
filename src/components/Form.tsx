a415908bbe81344dd20af354e255fd8b0271eb3e
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2024-08-04 02:07:41 -0500

    feat(form, Header, types): estructura form
    * en el header se da un pequeño estilo al boton reset
    *  en el form se crea el state de activity, y se mira que recibe cada campo
    * se especifica el tipo que es la actividad

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { categorie } from "../data/categories";
+import { Activity } from "../types";
 export default function Form() {
-  //el space-y-5 es un espacio entre los elementos hijos del form
-  //shadow es una sombra en el form
+  // no se definen varios states de categoria, actividad y calorias debido a que se puede hacer con un solo state, tambien de que todos ellos estan relacionados y dependen uno del otro
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+  //handleChange es una funcion que recibe un evento y cambia el estado de la actividad
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    // se crea una constante que verifica si el campo es un campo de numero o no
+    //* el includes es un metodo que verifica si un elemento esta dentro de un array
+    const isNumberField = ["calories", "category"].includes(e.target.id);
+    //se cambia el estado de la actividad
+    setActivity({
+      //se mantiene el estado anterior para no perder los valores anteriores
+      ...activity,
+      //se toma el target del evento y se lo asigna a dicho target
+      // el target.id es el id del elemento en el que se esta escribiendo y el target.value es lo que el usuario esta escribiendo
+      // la sintaxis es [key]: value y se coloca dentro de los corchetes para tener una key dinamica
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+    // esto tambien se hace para que gusrde el valor de lo que este asignado el usuario en el target
+  };
   return (
+    //el space-y-5 es un espacio entre los elementos hijos del form
+    //shadow es una sombra en el form
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-5">
         <label htmlFor="category" className="font-bold">
@@ -11,6 +37,9 @@ export default function Form() {
         <select
           className="border border-slate-300 p-2 rounded-lg w-full"
           id="category"
+          // si se tiene un value se debe tener un onChange
+          value={activity.category}
+          onChange={handleChange}
         >
           {categorie.map((category) => (
             <option key={category.id} value={category.id}>
@@ -20,14 +49,16 @@ export default function Form() {
         </select>
       </div>
       <div className="grid grid-cols-1 gap-5">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className="border border-slate-300 p-2 rounded-lg w-full"
           placeholder="Ejemplo: Comida, Jugo, Ensalada, Ejercicio, etc."
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
       <div className="grid grid-cols-1 gap-5">
@@ -39,6 +70,8 @@ export default function Form() {
           type="number"
           className="border border-slate-300 p-2 rounded-lg w-full"
           placeholder="Ejemplo: 100"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
       <button
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,12 @@ export default function Header() {
   return (
     <header className="bg-lime-600 py-3">
       <div className="max-w-4xl mx-auto flex justify-between">
-        <h1 className="text-center text-lg font-bold text-white uppercase">
+        <h1 className="text-center text-lg font-bold text-white uppercase px-6">
           Contador de Calorias
         </h1>
-        <button className="">Reset App</button>
+        <button className="px-3 mx-3 border-2 rounded-xl text-white hover:bg-lime-400 hover:text-black transition duration-300 ease-in-out">
+          Reset App
+        </button>
       </div>
     </header>
   );
